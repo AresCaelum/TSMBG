c30d8c512cdbc5bb6b805ad2f9caca432fd8102e
James Bean <JLBean1988@gmail.com>
2018-10-13 03:28:56 -0400

    Some Stuff...

---
+++ Assets/JBEANScripts/AutoDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDeath : MonoBehaviour {
+    public float minX = -13.0f;
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void Update()
+    {
+        if(transform.position.x < minX)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
