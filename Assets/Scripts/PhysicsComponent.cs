5b9ac4bf102b4e529dd2aabbfb81fa83ae2e7fa8
DeValere <erickdelariva6@gmail.com>
2018-10-13 02:09:39 -0400

    -Added player and all needed components. -Added new layers Ground and Player.

---
+++ Assets/Scripts/PhysicsComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PhysicsComponent : MonoBehaviour
+{
+    private Rigidbody2D _rb;//We cannot access this, used to store the rigidbody.
+    public Rigidbody2D RB//We can access this, acquires our rigidbody (if any) and returns it.
+    {
+        get
+        {
+            if (_rb == null)
+            {
+                _rb = gameObject.GetComponent<Rigidbody2D>();
+                if(_rb == null)
+                {
+                    _rb = gameObject.AddComponent<Rigidbody2D>();
+                }
+            }
+            return _rb;
+        }
+    }
+}
